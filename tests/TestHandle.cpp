34970f47cf5fecf8a21bf6c1ae2673233e0bd61b
Daniel Wiberg <wibbe@blueeye.se>
2013-05-31 00:24:06 +0200

    Added another handle test

--- tests/TestHandle.cpp
+++ tests/TestHandle.cpp
@@ -25,3 +25,16 @@ TEST(Handle, TypeConversion)
 
   CHECK_EQ((uint32_t)handle, 5);
 }
+
+TEST(Handle, Copy)
+{
+  gfx::Handle handle(1, 2, 3);
+  gfx::Handle copy = handle;
+
+  CHECK_EQ(handle.index, 1);
+  CHECK_EQ(handle.counter, 2);
+  CHECK_EQ(handle.type, 3);
+  CHECK_EQ(copy.index, 1);
+  CHECK_EQ(copy.counter, 2);
+  CHECK_EQ(copy.type, 3);
+}
